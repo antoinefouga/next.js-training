e0da1d5f326d1ae371f46a3be0e80121a76e38f3
antoinefouga <antoine.fouga1@gmail.com>
2024-12-13 14:41:52 +0100

    :goal_net: add error 404 not found

--- src/app/products/[id]/comments/[commentId]/page.tsx
+++ src/app/products/[id]/comments/[commentId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ProductComment({
   params,
 }: {
-  params: { id: string; commentId: string };
+  params: { id: string; commentId: string }
 }) {
+  if(parseInt(params.id) > 50){
+    notFound();
+  }
   return (
     <h1>
       Comment {params.commentId} for product {params.id}
