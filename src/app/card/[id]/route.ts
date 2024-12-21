3c1ad34cd536f95f5ff6fb91b7d9f712d6a43480
antoinefouga <antoine.fouga1@gmail.com>
2024-12-21 16:09:33 +0100

    Request Patch

--- src/app/card/[id]/route.ts
+++ src/app/card/[id]/route.ts
@@ -7,3 +7,15 @@ export async function GET(
   const card = cards.find((card) => card.id === parseInt(params.id));
   return Response.json(card);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const title = body.title;
+  const index = cards.findIndex((card) => card.id === parseInt(params.id));
+
+  cards[index].title = title;
+  return Response.json(cards[index].title);
+}
