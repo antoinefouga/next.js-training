387205e8bc25048b97b25a42c9089247fba7a673
antoinefouga <antoine.fouga1@gmail.com>
2024-12-21 15:32:56 +0100

    Post request

--- src/app/card/route.ts
+++ src/app/card/route.ts
@@ -1,5 +1,20 @@
-import { card } from "./data";
+import { cards } from "./data";
 
 export async function GET() {
-  return Response.json(card);
+  return Response.json(cards);
+}
+
+export async function POST(request: Request) {
+  const card = await request.json();
+  const newCard = {
+    id: cards.length + 1,
+    title: card.title,
+  };
+  cards.push(newCard);
+  return new Response(JSON.stringify(newCard), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
 }
